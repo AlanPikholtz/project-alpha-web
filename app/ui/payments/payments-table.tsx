9af764c04da7e9e407062789631d747f9a78d159
Alan Antar <alan.antarlerner@gmail.com>
2025-04-21 09:48:32 -0300

    Fixed build errors

--- app/ui/payments/payments-table.tsx
+++ app/ui/payments/payments-table.tsx
@@ -1,34 +1,33 @@
-import { Payment } from "@/app/lib/payments/types";
-import { ColumnDef } from "@tanstack/react-table";
-import React from "react";
+// import { ColumnDef } from "@tanstack/react-table";
+// import React from "react";
 
-const columns: ColumnDef<Payment>[] = [
-  {
-    accessorKey: "date",
-    header: "Fecha/Hora",
-    cell: ({ row }) => {
-      const formatted = new Date(row.getValue("date")).toLocaleString("es-AR");
-      return formatted;
-    },
-  },
-  {
-    accessorKey: "amount",
-    header: "Monto",
-    cell: ({ row }) => {
-      const amount = parseFloat(row.getValue("amount"));
-      const formatted = new Intl.NumberFormat("es-AR", {
-        // Argetina formatting
-        style: "currency",
-        currency: "ARS",
-      }).format(amount);
+// const columns: ColumnDef<Payment>[] = [
+//   {
+//     accessorKey: "date",
+//     header: "Fecha/Hora",
+//     cell: ({ row }) => {
+//       const formatted = new Date(row.getValue("date")).toLocaleString("es-AR");
+//       return formatted;
+//     },
+//   },
+//   {
+//     accessorKey: "amount",
+//     header: "Monto",
+//     cell: ({ row }) => {
+//       const amount = parseFloat(row.getValue("amount"));
+//       const formatted = new Intl.NumberFormat("es-AR", {
+//         // Argetina formatting
+//         style: "currency",
+//         currency: "ARS",
+//       }).format(amount);
 
-      return formatted;
-    },
-  },
-  { accessorKey: "currency", header: "Moneda" },
-  { accessorKey: "method", header: "Metodo" },
-  { accessorKey: "clientId", header: "ID Cliente" },
-];
+//       return formatted;
+//     },
+//   },
+//   { accessorKey: "currency", header: "Moneda" },
+//   { accessorKey: "method", header: "Metodo" },
+//   { accessorKey: "clientId", header: "ID Cliente" },
+// ];
 
 export default function PaymentsTable() {
   return <div>PaymentsTable</div>;
